9833488764f4491aa62bd0a56ae8ea201f896802
54u63 <antoine.boury1@gmail.com>
2025-08-12 13:14:11 +0200

    with comments everything tastes better

--- main.c
+++ main.c
@@ -32,28 +32,29 @@ int iterationLoop(int sockfd){
   int bRead = 0;
   size_t cmdLen = 0; //size of the effective element in the "cmd" Buffer
   
-  while(!end){
-    bRead = read(sockfd,chunk,BUFFLEN);
-    if(bRead == 0){
+  while(!end){//watching the END flag
+    bRead = read(sockfd,chunk,BUFFLEN);//recieving command
+    if(bRead == 0){//if nothing is read : error
       perror("Nothing was recieved\n");
       out+=1;
       goto _GracefullEnd;
     }
-    if(strcmp(chunk,"STOP\n")==0){
+    if(strcmp(chunk,"STOP\n")==0){//if the stop signal is recieved
       end+=1;
     }
-    memcpy(cmd,chunk,bRead);
-    while(checkEnd(chunk,bRead)){
-      printf("wee need to fill the buffer\n");
+    memcpy(cmd,chunk,bRead);//putting the first chunk in the cmd buffer
+    while(checkEnd(chunk,bRead)){ //fix me : need to properly reallocate the buffer if the command is bigger than the buffer size
+      printf("wee need to fill the buffer\n"); 
       break;
     }
-    printf("%s <- cmd\n",cmd);
-    memset(cmd,'\0',BUFFLEN);
+    printf("%s <- cmd\n",cmd); //printing the recieved element
+    memset(cmd,'\0',BUFFLEN); //reseting the memory to avoid overlap;
   }
   printf("Client has been Gracefully ShutDown\n");
 
 _GracefullEnd:
   free(chunk);
+  free(cmd);
   return out;
 }
 
